c7709e00f7fcb3589bec1c05bcd132792a9da8f7
Łukasz Dwórski <lukasz.dworski@nvt.pl>
2017-12-07 15:11:51 +0100

    remove tomcat lib

--- src/main/java/com/example/jenkinsdemo/HelloController.java
+++ src/main/java/com/example/jenkinsdemo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.jenkinsdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/hi")
 public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    public HelloController() {
+        LOGGER.info("Instance created");
+    }
+
+    @GetMapping
+    public String ping(){
+        return "pong";
+    }
 
     @GetMapping(value = "/{name}")
     public String sayHi(@PathVariable("name") String name) {
