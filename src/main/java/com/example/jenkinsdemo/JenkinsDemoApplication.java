a721560f8da9ebaeb156da98b6bddee063b3e14c
Łukasz Dwórski <lukasz.dworski@nvt.pl>
2017-12-11 10:51:07 +0100

    war file fix

--- src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,11 +2,18 @@ package com.example.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinsDemoApplication.class);
 	}
 }
